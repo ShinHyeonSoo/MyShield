dc542a9415f45d68da423f33beb7af5dfd8d4993
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-09-10 10:42:10 +0900

    Add End Logic
    게임종료 Logic 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager _Instance;
+
     public GameObject _Square;
+    public GameObject _EndPanel;
     public Text _TimeText;
+    public Text _NowScore;
+
+    bool _IsPlay = true;
 
     float _Time = 0.0f;
+
+    private void Awake()
+    {
+        if (_Instance == null)
+            _Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1.0f;
         InvokeRepeating("MakeSquare", 0f, 1f);    
     }
 
     // Update is called once per frame
     void Update()
     {
-        _Time += Time.deltaTime;
-        _TimeText.text = _Time.ToString("N2");
+        if (_IsPlay)
+        {
+            _Time += Time.deltaTime;
+            _TimeText.text = _Time.ToString("N2");
+        }
     }
 
     void MakeSquare()
     {
         Instantiate(_Square);
+    }
+
+    public void GameOver()
+    {
+        _IsPlay = false;
+        Time.timeScale = 0.0f;
+        _NowScore.text = _Time.ToString("N2");
+        _EndPanel.SetActive(true);
     }
 }
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -22,4 +22,12 @@ public class Square : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            GameManager._Instance.GameOver();
+        }
+    }
 }
