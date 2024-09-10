2a2bce42aee7b19684022c9749820024f4f14ede
ShinHyeonSoo <106219658+ShinHyeonSoo@users.noreply.github.com>
2024-09-10 10:12:45 +0900

    Add Timer
     타이머 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject _Square;
+    public Text _TimeText;
+
+    float _Time = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _Time += Time.deltaTime;
+        _TimeText.text = _Time.ToString("N2");
     }
 
     void MakeSquare()
